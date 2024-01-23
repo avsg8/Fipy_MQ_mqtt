555df9ebfd31eba42af6c2640359a1808e02c7ec
avsg8 <63816258+avsg8@users.noreply.github.com>
2024-01-23 13:01:09 -0500

    Update MQ7_main.py

--- MQ7_main.py
+++ MQ7_main.py
@@ -1,4 +1,5 @@
 # Test for MQ-series drivers
+# Ported from https://github.com/kartun83/micropython-MQ
 # --------------------------------------------------
 # Edited to run on PyCom devices
 # Author: avsg8, (https://github.com/avsg8), 01/2024
