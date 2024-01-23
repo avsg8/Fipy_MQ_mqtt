ad695d4297e281f96e7b1737c1014be8cc76360f
avsg8 <63816258+avsg8@users.noreply.github.com>
2024-01-23 12:57:55 -0500

    Update MQ7.py

--- lib/MQ7.py
+++ lib/MQ7.py
@@ -1,6 +1,9 @@
 #include "MQ7.h"
 # Ported from https://github.com/amperka/TroykaMQ
 # Author: Alexey Tveritinov [kartun@yandex.ru]
+# --------------------------------------------------
+# Edited to run on PyCom devices
+# Author: avsg8, (https://github.com/avsg8), 01/2024
 from BaseMQ import BaseMQ 
 from micropython import const
 
