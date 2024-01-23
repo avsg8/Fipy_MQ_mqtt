6f96d73336565d5537e76c7b45cfae674b9009db
avsg8 <63816258+avsg8@users.noreply.github.com>
2024-01-23 13:00:13 -0500

    Update boot.py

--- boot.py
+++ boot.py
@@ -2,7 +2,6 @@
 # --------------------------------------------------
 # Author: avsg8, (https://github.com/avsg8), 01/2024
 
-from BaseMQ import BaseMQ 
 from micropython import const
 import machine
 # select which "main" file you want to run once the device boots
