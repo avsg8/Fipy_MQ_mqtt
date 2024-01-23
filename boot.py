8ae84321def5f0493b55ff08fe74b5586e98d38e
avsg8 <63816258+avsg8@users.noreply.github.com>
2024-01-23 12:59:53 -0500

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,4 +1,9 @@
 # boot.py -- run on boot-up
+# --------------------------------------------------
+# Author: avsg8, (https://github.com/avsg8), 01/2024
+
+from BaseMQ import BaseMQ 
+from micropython import const
 import machine
 # select which "main" file you want to run once the device boots
 #machine.main('MQ7_main.py')
